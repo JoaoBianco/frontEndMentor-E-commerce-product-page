10bdb35f23c47e18a30e9d0c7d40d1347dcdd7bf
JoaoBianco <pepe.bianco@hotmail.com>
2023-03-02 13:51:34 -0300

    NavBar

---
+++ src/animation/animations.js
@@ -0,0 +1,44 @@
+export const icon = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(29, 32, 38,0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(29, 32, 38,1)",
+  },
+};
+
+export const cart = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(105, 112, 125,0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(105, 112, 125, 1)",
+  },
+};
+
+export const hambuguerMenu = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+export const hambuguerMenuItem = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+  },
+};
