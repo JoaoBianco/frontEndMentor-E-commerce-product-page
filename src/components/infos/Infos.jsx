449d018da1e84330fc2530377f5bd24bd0071e52
JoaoBianco <pepe.bianco@hotmail.com>
2023-03-02 16:23:10 -0300

    Cart logic

--- src/components/infos/Infos.jsx
+++ src/components/infos/Infos.jsx
@@ -4,8 +4,13 @@ import plus from "../../assets/icon-plus.svg";
 import { motion } from "framer-motion";
 import { rightSlide } from "../../animation/animations";
 
+import { v4 as uuidv4 } from "uuid";
+
 const Infos = () => {
   const [quantity, setQuantity] = useState(0);
+  const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+  console.log(cartItems);
 
   function changeQuantityHandler(type) {
     if (type === "plus") {
@@ -13,6 +18,11 @@ const Infos = () => {
     } else {
       quantity <= 0 ? setQuantity(0) : setQuantity(quantity - 1);
     }
+  }
+
+  function addToCart() {
+    let itensToAdd = [...cartItems, { id: uuidv4(), quantity: quantity }];
+    localStorage.setItem("cartItems", JSON.stringify(itensToAdd));
   }
 
   return (
@@ -59,7 +69,7 @@ const Infos = () => {
             <img src={plus} alt="plus" />
           </span>
         </div>
-        <button className="btn__primary">
+        <button className="btn__primary" onClick={addToCart}>
           <svg width="22" height="20" xmlns="http://www.w3.org/2000/svg">
             <path
               d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
